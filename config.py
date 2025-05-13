2e74c124984d8d055fd2bf5ad3d494246566cda0
amandasx0 <amandasarahcosta@gmail.com>
2025-05-13 15:33:30 -0300

    feat: PROD

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv # Traz a função para carregar as variaveis de a
 load_dotenv() # Carrega as variaveis de ambiente para esse arquivo
 
 class Config():
-    SQLALCHEMY_DATABASE_URI = environ.get("URL_DATABASE_DEV")
+    SQLALCHEMY_DATABASE_URI = environ.get("URL_DATABASE_PROD")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
